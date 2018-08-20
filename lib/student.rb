0eaec418f3f312e7f5d8150458c9bf0a80110fae
Maurice Argoetti <maurice.argoetti@gmail.com>
2018-08-20 23:41:15 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,8 +58,8 @@ class Student < InteractiveRecord
     DB[:conn].execute(sql)
   end
 
-  def self.find_by(column_names)
-    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{column_names}'"
+  def self.find_by(name:)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
     DB[:conn].execute(sql)
   end
 
